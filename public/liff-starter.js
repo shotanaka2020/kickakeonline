1eacbf551943ade49913d7a5d54414d7183c43de
shotanaka2020 <63491017+shotanaka2020@users.noreply.github.com>
2020-04-12 19:56:14 +0900

    Add files via upload

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "https://liff.line.me/1654069429-89lVzj1V";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1654069429-89lVzj1V";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
